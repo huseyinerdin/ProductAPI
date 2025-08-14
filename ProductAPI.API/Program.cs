1d7ef5bdc9ec4d156d9303aaea854c6a28a4d575
hüseyin erdin <erdinh61@gmail.com>
2025-08-14 15:26:14 +0300

    Dependecy injections added.

--- ProductAPI.API/Program.cs
+++ ProductAPI.API/Program.cs
@@ -1,6 +1,10 @@
+using ProductAPI.API.Middlewares;
+using ProductAPI.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ ProductAPI.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductAPI.Application.Services;
+using ProductAPI.Infrastructure;
+
+namespace ProductAPI.Application
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddInfrastructureServices(configuration);
+            services.AddScoped<IProductsService, ProductsService>();
+            return services;
+        }
+    }
+}
