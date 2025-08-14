dee607f649c5d9f5e2a97577898adb937697ddb0
hüseyin erdin <erdinh61@gmail.com>
2025-08-14 14:54:20 +0300

    Required Dtos added. Products Service added.

---
+++ ProductAPI.Application/Services/IProductsService.cs
@@ -0,0 +1,15 @@
+﻿using ProductAPI.Application.DTOs;
+
+namespace ProductAPI.Application.Services
+{
+    public interface IProductsService
+    {
+        Task<ProductResponseDto?> CreateAsync(ProductCreateDto dto);
+
+        Task<IEnumerable<ProductResponseDto>> GetAllAsync();
+
+        Task<ProductResponseDto?> GetByIdAsync(Guid id);
+
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
---
+++ ProductAPI.Application/Services/ProductsService.cs
@@ -0,0 +1,60 @@
+﻿using ProductAPI.Application.DTOs;
+using ProductAPI.Domain.Entities;
+using ProductAPI.Infrastructure.Repositories;
+
+namespace ProductAPI.Application.Services
+{
+    public class ProductsService(IGenericRepository<Product> productRepository) : IProductsService
+    {
+        private readonly IGenericRepository<Product> _productRepository = productRepository;
+        public async Task<ProductResponseDto?> CreateAsync(ProductCreateDto dto)
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price,
+            };
+            await _productRepository.AddAsync(product);
+            var result = await _productRepository.SaveChangesAsync();
+
+            return result ? MapToResponseDto(product) : null;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return false;
+            }
+            _productRepository.Remove(product);
+            var result = await _productRepository.SaveChangesAsync();
+            return result;
+        }
+
+        public async Task<IEnumerable<ProductResponseDto>> GetAllAsync()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return products.Select(MapToResponseDto);
+        }
+
+        public async Task<ProductResponseDto?> GetByIdAsync(Guid id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            return product == null ? null : MapToResponseDto(product);
+        }
+
+        private static ProductResponseDto MapToResponseDto(Product product)
+        {
+            return new ProductResponseDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price
+            };
+        }
+    }
+}
